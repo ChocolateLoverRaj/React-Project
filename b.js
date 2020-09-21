6c4ef632e3814867eb8a2dda91350a75e3dbef0a
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-09-21 15:02:01 -0500

    browserJs

--- b.js
+++ b.js
@@ -25,11 +25,8 @@ const build = async () => {
     .update(buff)
     .digest()
 
-  // If a file was changed, returns the changed hash
-  const getChangedBuff = async (inputPath, hashPath) => {
-    // The input hash
-    const inputHash = (async () => hash(await readFile(inputPath)))()
-
+  // If some hash doesn't match a hash stream, returns the changed hash
+  const getChangedBuffWithInputHash = async (inputHash, hashPath) => {
     // The hash stream
     const hashStream = createReadStream(hashPath)
 
@@ -53,6 +50,15 @@ const build = async () => {
         throw e
       }
     }
+  }
+
+  // If a file was changed, returns the changed hash
+  const getChangedBuffWithInputPath = async (inputPath, hashPath) => {
+    // Create an input hash promise
+    const inputHash = (async () => hash(await readFile(inputPath)))()
+
+    // Use the other func
+    return await getChangedBuffWithInputHash(inputHash, hashPath)
   }
 
   // The paths to the lib and dist dirs
@@ -102,6 +108,12 @@ const build = async () => {
         const inputJsPath = join(libPagePath, './index.js')
         const inputJsHashPath = join(distPagePath, './index-js-hash.dat')
 
+        // The browserJs path
+        const browserJsPath = join(distPagePath, './browser.js')
+        const browserJsHashPath = join(distPagePath, './browser-js-hash.dat')
+
+        // The instructionJ
+
         // Whether or not the page exists in the dist pages dir
         const distPageExists = (async () => await distPagesExists && (await distPagesDir).includes(page))()
 
@@ -112,28 +124,118 @@ const build = async () => {
           }
         })()
 
-        // The changed buff
-        const inputJsHash = (async () => await distPageExists
-          ? await getChangedBuff(inputJsPath, inputJsHashPath)
-          : hash(await readFile(inputJsPath))
-        )()
+        // Write the inputJsHash
+        const {
+          inputJsHash,
+          writeInputJsHash
+        } = (() => {
+          // The changed buff
+          const changedBuff = getChangedBuffWithInputPath(inputJsPath, inputJsHashPath)
 
-        // Write the changed buff
-        const writeInputJsHash = (async () => {
-          const buff = await inputJsHash
-          if (buff) {
-            // Wait for the createPageDir promise
-            console.log('changes', page)
-            await createPageDir
-            await writeFile(inputJsHashPath, buff)
-          } else {
-            console.log('no changes', page)
+          // The new hash
+          const newHash = (async () => hash(await readFile(inputJsPath)))()
+
+          // The changed buff
+          const inputJsHash = (async () => await distPageExists
+            ? await changedBuff
+            : await newHash
+          )()
+
+          // Write to the hash files
+          const writeInputJsHash = (async () => {
+            const buff = await inputJsHash
+            if (buff) {
+              // Wait for the createPageDir promise
+              console.log('changes', 'index.js', page)
+              await createPageDir
+              await writeFile(inputJsHashPath, buff)
+            } else {
+              console.log('no changes', 'index.js', page)
+            }
+          })()
+
+          // Return the inputJsHash and the writePromise
+          return {
+            inputJsHash,
+            writeInputJsHash
           }
         })()
 
-        // Wait for the inputJsHash to be written
+        // The rollup input
+        const bundle = rollup({
+          input: inputJsPath,
+          external: 'react',
+          plugins: [babelPlugin]
+        })
 
-        await writeInputJsHash
+        // Write browserJs and browserJsHash
+        const buildBrowserJs = (async () => {
+          // BrowserJs Code
+          const browserJsCode = (async () => {
+            // Build browserJs
+            const buildBrowserJs = (async () => (await bundle).generate({
+              format: 'es',
+              paths: {
+                react: '/scripts/react.js'
+              }
+            }))()
+
+            // Get the code from the first output
+            const codePromise = (async () => (await buildBrowserJs).output[0].code)()
+
+            // Wait for the codePromise
+            return await codePromise
+          })()
+
+          // The hash, if changed
+          const browserJsHash = (async () => {
+            // BrowserJs Hash
+            const newBrowserJsHash = (async () => hash(await browserJsCode))()
+
+            // The changed buff
+            const changedBrowserJsBuff = getChangedBuffWithInputHash(newBrowserJsHash, browserJsHashPath)
+
+            // The hash depending on if the distPage exists
+            const hashToUse = (async () => await distPageExists
+              ? await changedBrowserJsBuff
+              : await newBrowserJsHash
+            )()
+
+            // Wait for the hashToUse promise
+            return await hashToUse
+          })()
+
+          // Write the browserJsHash
+          const writeBrowserJsHash = (async () => {
+            // Remember that if the inputJsHash is unchanged there is no need to build this
+            const buff = await inputJsHash && await browserJsHash
+            if (buff) {
+              console.log('changes', 'browser.js', page)
+              await createPageDir
+              await writeFile(browserJsHashPath, buff)
+            } else {
+              console.log('no changes', 'browser.js', page)
+            }
+          })()
+
+          // Write browserJs
+          const writeBrowserJs = (async () => {
+            // Check that the inputJs and browserJs hashes have changed
+            if (await inputJsHash && await browserJsHash) {
+              await createPageDir
+              await writeFile(browserJsPath, await browserJsCode)
+            }
+          })()
+
+          // Wait for file and hash to be written
+          await Promise.all([writeBrowserJsHash, writeBrowserJs])
+        })()
+
+        // Wait for the necessary tasks
+        await Promise.all([
+          writeInputJsHash,
+          buildBrowserJs
+        ])
       })())
     }
 
@@ -171,205 +273,6 @@ const build = async () => {
       }
     }
   })()
-}
-
-const buildZ = async () => {
-  // Check hashes
-  // If the index.js file has changed, then redo the output index.js, app.html
-  // If the index.html file has changed, then redo the index.html
-  // If the app.html has changed, redo the index.html
-
-  const libPagesPath = join(__dirname, './lib/browser/components/pages/')
-  const distPagesPath = join(__dirname, './dist/browser/components/pages/')
-
-  let noDistPages = false
-  const [libPages, distPages] = await Promise.all([
-    // Read the lib/browser/components/pages/ dir
-    readdir(libPagesPath).catch(err => {
-      if (err.code === 'ENOENT') {
-        throw new Error('Lib pages folder doesn\'t exist.')
-      } else {
-        throw err
-      }
-    }),
-
-    // Read the dist/browser/components/pages dir
-    readdir(distPagesPath).catch(err => {
-      if (err.code === 'ENOENT') {
-        noDistPages = true
-      } else {
-        throw err
-      }
-    })
-  ])
-
-  // Loop through the lib pages
-  for (const page of libPages) {
-    (async () => {
-      const noDistPage = noDistPages || !distPages.includes(page)
-
-      const distPagePath = join(distPagesPath, `./${page}/`)
-      const libPagePath = join(libPagesPath, `./${page}/`)
-
-      const inputJsHashPath = join(distPagePath, './input-js-hash.dat')
-      const inputJsPath = join(libPagePath, './index.js')
-      const inputJsRefsPath = join(libPagePath, './input-js-files')
-
-      const browserJsHashPath = join(distPagePath, './browser-js-hash.dat')
-      const browserJsPath = join(distPagePath, './browser.js')
-
-      const buildJs = async () => {
-        const bundle = rollup({
-          input: inputJsPath,
-          external: 'react',
-          plugins: [babelPlugin]
-        })
-
-        const generateBrowserJs = (async () => {
-          const outputCode = (async () => {
-            return (await (await bundle).generate({
-              format: 'es',
-              paths: {
-                react: '/scripts/react.js'
-              }
-            })).output[0].code
-          })()
-
-          const outputHash = (async () => {
-            return createHash('sha256')
-              .update(await outputCode)
-              .digest()
-          })()
-
-          const writeBrowserJs = async () => {
-            const writeOutputHash = (async () => {
-              await writeFile(browserJsHashPath, await outputHash)
-            })()
-
-            const writeBrowserJs = (async () => {
-              writeFile(browserJsPath, await outputCode)
-            })()
-
-            await Promise.all([writeOutputHash, writeBrowserJs])
-            console.log('done writing browserJs')
-          }
-
-          if (!noDistPage) {
-            const browserJsHash = createReadStream(browserJsHashPath)
-            try {
-              const different = await compareBufferStream(outputHash, browserJsHash)
-              if (different) {
-                await writeBrowserJs()
-              } else {
-                console.log('same hash')
-              }
-            } catch (e) {
-              if (e.code === 'ENOENT') {
-                console.log('no hash')
-                await writeBrowserJs()
-              } else {
-                throw e
-              }
-            }
-          } else {
-            await writeBrowserJs()
-          }
-        })()
-
-        const generateInstructionJs = (async () => {
-          const output = (async () => {
-            return (await ((await bundle).generate({
-              format: 'es'
-            }))).output[0]
-          })()
-
-          if (!noDistPage) {
-
-          }
-        })()
-
-        await Promise.all([generateBrowserJs, generateInstructionJs])
-      }
-
-      if (!noDistPage) {
-        const getChangedBuff = async (inputPath, hashPath) => new Promise((resolve, reject) => {
-          const oldInputHash = createReadStream(hashPath)
-          const input = createReadStream(inputPath)
-          const newInputHash = input.pipe(createHash('sha256'))
-          const newInputHashBuff = (async () => {
-            return (await once(newInputHash, 'data'))[0]
-          })()
-
-          oldInputHash.on('error', err => {
-            if (err.code === 'ENOENT') {
-              resolve(newInputHashBuff)
-            } else {
-              reject(err)
-              input.destroy(err)
-            }
-          })
-
-          input.on('error', err => {
-            if (err.code === 'ENOENT') {
-              reject(new Error('Input js file doesn\'t exist.'))
-            } else {
-              reject(err)
-            }
-            oldInputHash.destroy(err)
-          })
-
-          newInputHash.on('error', err => {
-            reject(err)
-          })
-
-          areStreamsSame(oldInputHash, newInputHash).then(({ same }) => {
-            if (same) {
-              resolve(false)
-            } else {
-              resolve(newInputHashBuff)
-            }
-          })
-        })
-
-        const inputJsChangedBuff = getChangedBuff(inputJsPath, inputJsHashPath)
-        const updateInputJsHash = (async () => {
-          const buff = await inputJsChangedBuff
-          if (buff) {
-            await writeFile(inputJsHashPath, buff)
-          }
-        })()
-        const updateRefHashPromises = []
-        const refFilesChanged = (async () => {
-          try {
-            const inputJsRefs = (await readFile(inputJsRefsPath))
-            console.log(JSON.parse(inputJsRefs))
-          } catch (e) {
-            if (e.code === 'ENOENT') {
-              console.log('no refs')
-              return true
-            } else {
-              throw e
-            }
-          }
-        })()
-
-        if (await firstTruthy([inputJsChangedBuff, refFilesChanged])) {
-          await Promise.all([updateInputJsHash, ...updateRefHashPromises, buildJs()])
-          console.log('rebuilt')
-        } else {
-          console.log('no inputs changed')
-        }
-      } else {
-        await mkdir(distPagePath, { recursive: true })
-
-        const hashStream = createReadStream(inputJsPath)
-          .pipe(createHash('sha256'))
-          .pipe(createWriteStream(inputJsHashPath))
-
-        await Promise.all([once(hashStream, 'finish'), buildJs()])
-      }
-    })()
-  }
 }
 
 console.log('Starting Build')
